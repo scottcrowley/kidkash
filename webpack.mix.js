0b486079ae1cb8ec82963e95911289a329241f7a
= <=>
2021-08-19 07:54:05 -0700

    Update Laravel Mix and it's dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,7 @@ const mix = require('laravel-mix');
 //    .sass('resources/sass/app.scss', 'public/css');
 
 mix.js('resources/js/app.js', 'public/js')
+   .vue()
    .postCss('resources/css/app.css', 'public/css', [
       require('postcss-import'),
       require('tailwindcss'),
