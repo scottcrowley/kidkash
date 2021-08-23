52814bfaa74e12ffd72c5bbd509e2a1a7ef80e9d
= <=>
2021-08-23 11:44:08 -0700

    Update tailwind.css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 const { colors } = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
   theme: {
     extend: {
       fontFamily: {
